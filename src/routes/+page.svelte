1b0ddbd2047d88a050ee37a9ffa07400930fd959
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2022-08-31 17:41:03 +0900

    basic header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header class="container mx-auto flex justify-between py-4">
+  <div class="flex items-center">
+    <!-- color blocks -->
+    <span class="block w-8 h-8 rounded-full bg-gray-800 mr-2"></span>
+    <span class="block w-8 h-8 rounded bg-blue-400 mr-2"></span>
+    <span class="block w-8 h-8 rounded bg-orange-600 mr-2"></span>
+
+    <h1 class="flex align-items">
+      <span>MIDI.</span>
+      <span>Link</span>
+    </h1>
+  </div>
+
+  <!-- Open app button -->
+  <a href="#" class="" target="_blank">Open app</a>
+  
+</header>
+
+<main class="container mx-auto">
+
+  <section></section>
+
+  <section></section>
+
+  <section></section>
+
+</main>
